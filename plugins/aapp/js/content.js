ede88a590e8c973c933571102ae526ad0ad29e3f
Jo Ruby P <jorubyp@gmail.com>
2019-03-15 16:09:56 +0000

    hfrrhrhr

--- plugins/aapp/js/content.js
+++ plugins/aapp/js/content.js
@@ -1,7 +1,9 @@
 (function(){
-    $(document).bind('DOMNodeInserted', function(event) {
-        $.each($(event.target).find('.audio-image'), function(i,e){
-            $(e).addClass('play-pause');
+    $(document).ready(function(){
+        $(document).bind('DOMNodeInserted', function(event) {
+            $.each($(event.target).find('.audio-image'), function(i,e){
+                $(e).addClass('play-pause');
+            })
         })
-    })
+    });
 })
