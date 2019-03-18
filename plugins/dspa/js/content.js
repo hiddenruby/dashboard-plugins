11619f5456a6445360c8c5413b4d601da703e16d
Jo Ruby P <jorubyp@gmail.com>
2019-03-18 10:46:47 +0000

    uutrutrrtya

--- plugins/dspa/js/content.js
+++ plugins/dspa/js/content.js
@@ -6,7 +6,7 @@
                     postBottom = $('.post_dismiss').parents().eq(5).offset().top + $('.post_dismiss').parents().eq(5).outerHeight() + 300;
 
                 if (postBottom < top){
-                    $('.post_dismiss[data-dashboardplugins-influencer^="aapp"').attr({'data-dashboardplugins-influencer':'aapp'}).children().last()[0].click()
+                    $('.post_dismiss[data-dashboardplugins-influencer^="aapp"]').attr({'data-dashboardplugins-influencer':'aapp'}).children().last()[0].click()
                 };
             }
         })
