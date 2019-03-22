fcac69e9fa8a50b6bad5bcb957d91100ea8dd17a
Jo Ruby P <jorubyp@gmail.com>
2019-03-22 17:36:51 +0000

    toggle test 3

--- plugins/eaud/js/toggle.js
+++ plugins/eaud/js/toggle.js
@@ -1,5 +1,5 @@
 (function($,pluginId,btnToggle){
-    $(document).on('click', btnToggle, function(){
+    $(document).on('click', '#user_enable_eaud', function(){
         $('body').toggleClass('flag--accessibility-design-update', !$('flag--accessibility-design-update').length);
     })
 })(jQuery)
