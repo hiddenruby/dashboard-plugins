ad25f846707f7199f78029cc7e33d817b80b1b15
Jo Ruby P <jorubyp@gmail.com>
2019-03-28 17:28:16 +0000

    vccvbvccv

--- plugins/dspa/js/content.js
+++ plugins/dspa/js/content.js
@@ -2,7 +2,7 @@
     $(window).scroll(() => {
         $.each($('.post_container').find('.post_dismiss'), (i,e) => {
             let top = $(window).scrollTop(),
-                container = $(e).closest('post_container'),
+                container = $(e).closest('.post_container'),
                 containerBottom = $(container).offset().top + $(container).outerHeight();
             $(container).find('.post').addClass('is_audio');
             if (containerBottom + 300 < top){
