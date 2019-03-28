0fb31d72a2d0219bce355894cf57f37e24a7ab58
Jo Ruby P <jorubyp@gmail.com>
2019-03-28 15:03:58 +0000

    rgererer

--- plugins/dspa/js/content.js
+++ plugins/dspa/js/content.js
@@ -1,21 +1,23 @@
-$(document).bind('DOMNodeInserted', (event) => {
-    console.log('ay')
-    $(event.target).find('.post_dismiss').closest('.post_container').attr({'data-dashboardplugins-influencer': 'dspa'})
-});
-let scrollTop = $(window).scrollTop();
-$(window).scroll(() => {
-    $.each($('*[data-dashboardplugins-influencer="dspa"]'), (i) => {
-        console.log('bastard spotted',$(this).find('.post').attr('data-id'))
-        let postBottom = $(this).offset().scrollTop + $(this).outerHeight();
-        if (postBottom < scrollTop){
-            console.log('preparing attack',$(this).find('.post').attr('data-id'))
-            $(this).find('.post').addClass('is_audio');
-            setTimeout(() => {
-                if (postBottom < scrollTop){
-                    console.log('taking out target',$(this).find('.post').attr('data-id'))
-                    $(this).find('.post_dismiss').click()
-                };
-            },5000)
-        };
+(function($){
+    $(document).bind('DOMNodeInserted', (event) => {
+        console.log('ay')
+        $(event.target).find('.post_dismiss').closest('.post_container').attr({'data-dashboardplugins-influencer': 'dspa'})
     });
-});
+    let top = $(window).scrollTop();
+    $(window).scroll(() => {
+        $.each($('*[data-dashboardplugins-influencer="dspa"]'), (i) => {
+            console.log('bastard spotted',$(this).find('.post').attr('data-id'))
+            let postBottom = $(this).offset().top + $(this).outerHeight();
+            if (postBottom < top){
+                console.log('preparing attack',$(this).find('.post').attr('data-id'))
+                $(this).find('.post').addClass('is_audio');
+                setTimeout(() => {
+                    if (postBottom < top){
+                        console.log('taking out target',$(this).find('.post').attr('data-id'))
+                        $(this).find('.post_dismiss').click()
+                    };
+                },5000)
+            };
+        });
+    });
+})(jQuery)
