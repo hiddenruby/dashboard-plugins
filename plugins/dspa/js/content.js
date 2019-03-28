4a0249b954e5af65e832c8e8da2d12c0b67cc00b
Jo Ruby P <jorubyp@gmail.com>
2019-03-28 17:24:04 +0000

    dfgdfwwww

--- plugins/dspa/js/content.js
+++ plugins/dspa/js/content.js
@@ -2,8 +2,8 @@
     $(window).scroll(() => {
         $.each($('.post_container').find('.post_dismiss'), (i,e) => {
             let top = $(window).scrollTop(),
-            container = $(e).closest('post_container');
-            containerBottom = container.offset().top + container.outerHeight();
+                container = $(e).closest('post_container'),
+                containerBottom = $(container).offset().top + $(container).outerHeight();
             $(container).find('.post').addClass('is_audio');
             if (containerBottom + 300 < top){
                 $(e).find('.icon_close').click()
