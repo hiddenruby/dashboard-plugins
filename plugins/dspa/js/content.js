eb194e146d3bb70ef1cd2ae3080849412f8e171c
Jo Ruby P <jorubyp@gmail.com>
2019-03-28 13:06:11 +0000

    dspa rework

--- plugins/dspa/js/content.js
+++ plugins/dspa/js/content.js
@@ -1,16 +1,20 @@
 (function($){
     $(document).bind('DOMNodeInserted', function(event) {
-        $.each($(event.target).find('.post_dismiss'), function(i,e){
-            $(window).scroll(function(){
-                if ($('.post_dismiss').length) {
-                    let top = $(window).scrollTop(),
-                        post = $('.post_dismiss').closest('.post_container'),
-                        postBottom = post.offset().top + post.outerHeight() + 300;
+        $(event.target).find('.post_dismiss').closest('.post_container').attr({'data-dashboardplugins-influencer': 'dspa'})
+    });
+    let top = $(window).scrollTop();
+    $(window).scroll(function(){
+        $.each($('*[data-dashboardplugins-influencer="dspa"]'), function(i,post) {
+            let btn = post.find('.post_dismiss'),
+                postBottom = post.offset().top + post.outerHeight();
+            if (postBottom < top){
+                post.addClass('is_audio')
+                setTimeout(()=>{
                     if (postBottom < top){
-                        $('.post_dismiss .icon_close[data-dashboardplugins-influencer!="aapp"]').attr({'data-dashboardplugins-influencer':'aapp'}).click()
+                        btn.click()
                     };
-                }
-            })
-        })
-    })
+                },5000)
+            };
+        });
+    });
 })(jQuery)
