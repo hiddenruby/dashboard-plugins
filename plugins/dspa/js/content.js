563af31aa0ba69b27854a26e73d0fa651b2bd912
Jo Ruby P <jorubyp@gmail.com>
2019-03-28 14:10:36 +0000

    dwdwd

--- plugins/dspa/js/content.js
+++ plugins/dspa/js/content.js
@@ -1,20 +1,22 @@
-$(document).bind('DOMNodeInserted', (event) => {
-    $(event.target).find('.post_dismiss').closest('.post_container').attr({'data-dashboardplugins-influencer': 'dspa'})
-});
-let top = $(window).scrollTop();
-$(window).scroll(() => {
-    $.each($('*[data-dashboardplugins-influencer="dspa"]'), (i) => {
-        console.log('bastard spotted',$(this).find('.post').attr('data-id'))
-        let postBottom = $(this).offset().top + $(this).outerHeight();
-        if (postBottom < top){
-            console.log('preparing attack',$(this).find('.post').attr('data-id'))
-            $(this).find('.post').addClass('is_audio');
-            setTimeout(() => {
-                if (postBottom < top){
-                    console.log('taking out target',$(this).find('.post').attr('data-id'))
-                    $(this).find('.post_dismiss').click()
-                };
-            },5000)
-        };
+(() => {
+    $(document).bind('DOMNodeInserted', (event) => {
+        $(event.target).find('.post_dismiss').closest('.post_container').attr({'data-dashboardplugins-influencer': 'dspa'})
     });
-});
+    let top = $(window).scrollTop();
+    $(window).scroll(() => {
+        $.each($('*[data-dashboardplugins-influencer="dspa"]'), (i) => {
+            console.log('bastard spotted',$(this).find('.post').attr('data-id'))
+            let postBottom = $(this).offset().top + $(this).outerHeight();
+            if (postBottom < top){
+                console.log('preparing attack',$(this).find('.post').attr('data-id'))
+                $(this).find('.post').addClass('is_audio');
+                setTimeout(() => {
+                    if (postBottom < top){
+                        console.log('taking out target',$(this).find('.post').attr('data-id'))
+                        $(this).find('.post_dismiss').click()
+                    };
+                },5000)
+            };
+        });
+    });
+})
