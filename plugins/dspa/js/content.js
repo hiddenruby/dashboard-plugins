1994d43259c405154b58126cfe0a12fb4a68758e
Jo Ruby P <jorubyp@gmail.com>
2019-03-28 15:28:28 +0000

    www

--- plugins/dspa/js/content.js
+++ plugins/dspa/js/content.js
@@ -1,19 +1,18 @@
 (function($){
     $(document).bind('DOMNodeInserted', (event) => {
-        console.log('rrr')
-        $(event.target).find('.post_dismiss').closest('.post_container').attr({'data-dashboardplugins-influencer': 'dspa'})
+        console.log($(event.target).find('.post_dismiss').length ? $('*[data-dashboardplugins-influencer="dspa"]').length ? 'bastard spotted' : '' : 'searching...')
+        $(event.target).find('.post_dismiss').closest('.post_container').attr({'data-dashboardplugins-influencer': 'dspa'});
     });
     $(window).scroll(() => {
         $.each($('*[data-dashboardplugins-influencer="dspa"]'), (i,e) => {
-            console.log('bastard spotted',$(e).find('.post').attr('data-id'))
             let top = $(window).scrollTop(),
                 postBottom = $(e).offset().top + $(e).outerHeight();
             if (postBottom < top){
-                console.log('preparing attack',$(e).find('.post').attr('data-id'))
+                console.log(!$(e).find('.is_audio') ? 'preparing attack ' + $(e).find('.post').attr('data-id') : '')
                 $(e).find('.post').addClass('is_audio');
                 setTimeout(() => {
                     if (postBottom < top){
-                        console.log('taking out target',$(e).find('.post').attr('data-id'))
+                        console.log('taking out target',$(e).find('.post_dismiss'))
                         $(e).find('.post_dismiss').click()
                     };
                 },5000)
