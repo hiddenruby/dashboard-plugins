d83f0a8f1274a8f874a795febc8aea2f693cb7a3
Jo Ruby P <jorubyp@gmail.com>
2019-03-28 13:43:13 +0000

    sdssdsd

--- plugins/dspa/js/content.js
+++ plugins/dspa/js/content.js
@@ -1,16 +1,17 @@
 (function($){
-    $(document).bind('DOMNodeInserted', function(event) {
+    $(document).bind('DOMNodeInserted', (event) => {
         $(event.target).find('.post_dismiss').closest('.post_container').attr({'data-dashboardplugins-influencer': 'dspa'})
     });
     let top = $(window).scrollTop();
-    $(window).scroll(function(){
-        $.each($('*[data-dashboardplugins-influencer="dspa"]'), function() {
+    $(window).scroll(() => {
+        $.each($('*[data-dashboardplugins-influencer="dspa"]'), () => {
             console.log($(this))
             let btn = $(this).find('.post_dismiss'),
                 postBottom = $(this).offset().top + $(this).outerHeight();
             if (postBottom < top){
                 $(this).find('.post').addClass('is_audio');
-                setTimeout(()=>{
+                setTimeout(() => {
+                    console.log(btn)
                     if (postBottom < top){
                         btn.click()
                     };
