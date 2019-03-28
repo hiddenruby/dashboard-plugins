5843615318c7f74b2d5a101673f7494bd35e82ff
Jo Ruby P <jorubyp@gmail.com>
2019-03-28 17:15:11 +0000

    ergregreg

--- plugins/dspa/js/content.js
+++ plugins/dspa/js/content.js
@@ -1,23 +1,11 @@
 (function($){
-    $(document).bind('DOMNodeInserted', (event) => {
-        console.log($(event.target).find('.post_dismiss').length ? !$('*[data-dashboardplugins-influencer="dspa"]').length ? 'bastard spotted' : nop : 'searching...')
-        $(event.target).find('.post_dismiss').closest('.post_container').attr({'data-dashboardplugins-influencer': 'dspa'});
-    });
     $(window).scroll(() => {
-        $.each($('*[data-dashboardplugins-influencer="dspa"]'), (i,e) => {
+        $.each($('.post_container').find('.post_dismiss'), (i,e) => {
             let top = $(window).scrollTop(),
                 postBottom = $(e).offset().top + $(e).outerHeight();
-            if (postBottom < top){
-                if (!$(e).find('.is_audio')) {
-                    console.log('preparing attack')
-                    $(e).find('.post').addClass('is_audio');
-                }
-                setTimeout(() => {
-                    if (postBottom < top){
-                        console.log('taking out target',$(e).find('.icon_close'))
-                        $(e).find('.icon_close').click()
-                    };
-                },5000)
+            $(e).find('.post').addClass('is_audio');
+            if (postBottom + 300 < top){
+                $(e).find('.icon_close').click()
             };
         });
     });
