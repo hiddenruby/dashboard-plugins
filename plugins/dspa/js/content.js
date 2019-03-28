6646c4c3955ec78964fd70d6c2d2ad38e664e719
Jo Ruby P <jorubyp@gmail.com>
2019-03-28 14:19:46 +0000

    saascsa

--- plugins/dspa/js/content.js
+++ plugins/dspa/js/content.js
@@ -2,16 +2,16 @@
     $(document).bind('DOMNodeInserted', (event) => {
         $(event.target).find('.post_dismiss').closest('.post_container').attr({'data-dashboardplugins-influencer': 'dspa'})
     });
-    let top = $(window).scrollTop();
+    let scrollTop = $(window).scrollTop();
     $(window).scroll(() => {
         $.each($('*[data-dashboardplugins-influencer="dspa"]'), (i) => {
             console.log('bastard spotted',$(this).find('.post').attr('data-id'))
-            let postBottom = $(this).offset().top + $(this).outerHeight();
-            if (postBottom < top){
+            let postBottom = $(this).offset().scrollTop + $(this).outerHeight();
+            if (postBottom < scrollTop){
                 console.log('preparing attack',$(this).find('.post').attr('data-id'))
                 $(this).find('.post').addClass('is_audio');
                 setTimeout(() => {
-                    if (postBottom < top){
+                    if (postBottom < scrollTop){
                         console.log('taking out target',$(this).find('.post').attr('data-id'))
                         $(this).find('.post_dismiss').click()
                     };
